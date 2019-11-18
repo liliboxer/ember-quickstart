c0b02f46297d42bb3eff40eaebfccfd0b377bb96
Lili Boxer <ninaliboxer@gmail.com>
2019-11-18 13:21:53 -0800

    cat route

--- app/routes/cats.js
+++ app/routes/cats.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return ['Garfield', 'Max', 'Felix', 'Hello Kitty', 'Simba', 'Nala', 'Puss in Boots', 'Cheshire', ]
+  }
 });
